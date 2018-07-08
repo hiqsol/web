a7ad886c6cc53022a64af9d95158aacfa0c1a150
Andrii Vasyliev <sol@hiqdev.com>
2018-07-08 14:29:55 +0300

    added core components to config

--- src/config/web.php
+++ src/config/web.php
@@ -1,4 +1,40 @@
 <?php
 
 return [
+    \yii\web\Application::class => \yii\di\Reference::to('application'),
+    'application' => [
+        '__class' => \yii\web\Application::class,
+        'id' => 'web',
+        'name' => 'web',
+    ],
+
+    'assetManager' => [
+        '__class' => \yii\web\AssetManager::class,
+    ],
+    'urlManager' => [
+        '__class' => \yii\web\UrlManager::class,
+    ],
+    'view' => [
+        '__class' => \yii\web\View::class,
+    ],
+    'request' => [
+        '__class' => \yii\web\Request::class,
+    ],
+    'response' => [
+        '__class' => \yii\web\Response::class,
+    ],
+    'session' => [
+        '__class' => \yii\web\Session::class,
+    ],
+    'user' => [
+        '__class' => \yii\web\User::class,
+    ],
+    'errorHandler' => [
+        '__class' => \yii\web\ErrorHandler::class,
+    ],
+
+    /// TODO: move to swiftmailer
+    'mailer' => [
+        '__class' => \yii\swiftmailer\Mailer::class,
+    ],
 ];
